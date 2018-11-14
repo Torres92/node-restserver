8e5ea1fa29f53a10b13b340eb5aaa56a7474f71b
Torres92 <aleph2445@gmail.com>
2018-11-14 11:03:34 -0500

    Npm start listo

--- server/server.js
+++ server/server.js
@@ -1,6 +1,10 @@
 require('./config/config');
 
 const express = require('express');
+// Using Node.js `require()`
+const mongoose = require('mongoose');
+
+
 const app = express();
 
 const bodyParser = require('body-parser');
@@ -10,43 +14,18 @@ app.use(bodyParser.urlencoded({ extended: false }))
  
 // parse application/json
 app.use(bodyParser.json())
-  
-app.get('/usuario', function (req, res) {
-  res.json('get usuario')
-});
 
-app.post('/usuario', function (req, res) {
-  
-  let body = req.body;
+app.use( require('./routes/usuario'))  
 
-  if (body.nombre === undefined ){
-
-  	res.status(400).json({
-  		ok: false,
-  		mensaje: "El nombre es necesario"
-  	});
-
-  }else {
-
-  res.json({
-  	persona:body
-  });
-  }
-});
-
-app.put('/usuario/:id', function (req, res) {
-  
-  let id = req.params.id;
-
-  res.json({
-  	id
-  });
-});
-
-app.delete('/usuario', function (req, res) {
-  res.json('delete usuario')
-});
  
+mongoose.connect('mongodb://localhost:27017/dtex', (err, res)=>{
+
+	if( err ) throw err;
+
+	console.log('BASE de datos ONLINE')
+});
+
+
 app.listen(process.env.PORT, ()=> {
 	console.log('Escuchando los cambios en puerto', process.env.PORT);
 });
